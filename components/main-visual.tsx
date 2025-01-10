b6bb1d4d962a9bea4b108f67d4b20d314eefb0d2
Toshiaki Yanagisawa <toshiaki@ToshiakinoMac-mini.local>
2025-01-10 11:12:33 +0900

    fixed main visual design

--- components/main-visual.tsx
+++ components/main-visual.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Logo } from '@/components/logo'
 import { useWindowSize } from "@/hooks/use-window-size"
 import Image from "next/image"
 
@@ -10,8 +11,9 @@ export const MainVisual = () => {
       {
         isMobile ? <Image src="./main_visual_sp.png" alt="main visual" fill objectFit="cover" /> : <Image src="./main_visual_pc.png" alt="main visual" fill objectFit="cover" />
       }
-      <div className='h-screen w-screen bg-slate-100 absolute bg-opacity-40'>
-        <p className="absolute text-black text-3xl top-60 left-1/3">text</p>
+      <div className='h-screen w-screen bg-white absolute bg-opacity-80 flex items-center justify-center flex-col gap-4'>
+        <p className="text-gray-600 text-3xl tracking-wider font-bold">遊び心で、機能以上の感動を</p>
+        <Logo />
       </div>
     </div>
   )
