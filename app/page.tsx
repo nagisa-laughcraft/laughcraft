a35338b79e05a7e5b9e77701c9e7641886db71c3
nagisa-laughcraft <yanagisawa@laughcraft.dev>
2025-01-16 10:33:01 +0900

    fixed main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,47 @@
 import { MainVisual } from '@/components/main-visual'
+import { Title } from '@/components/title'
+import { Description } from '@/components/description'
 import { About } from '@/components/about'
+import { Values } from '@/components/values'
 import { Skills } from '@/components/skills'
 import { Projects } from '@/components/projects'
-import { Title } from '@/components/title'
-import { Container } from '@/components/container'
+import { Contact } from '@/components/contact'
 
 export default async function Home() {
+  // animate-disableScroll を戻す
   return (
-    <div className="h-screen snap-y snap-mandatory scroll-smooth animate-disableScroll bg-mainVisualSp md:bg-mainVisualPc bg-cover">
+    <main className="h-screen snap-y snap-mandatory scroll-smooth bg-mainVisualSp md:bg-mainVisualPc bg-cover">
       <div className="h-screen bg-white bg-opacity-80 snap-start">
         <MainVisual />
       </div>
-      <Container>
-        <Title>About Me</Title>
-        <About />
-      </Container>
-      <Container>
-        <Title>Skills</Title>
-        <Skills />
-      </Container>
-      <Container>
-        <Title>Projects</Title>
-        <Projects />
-      </Container>
-    </div>
+      <div className="bg-white bg-opacity-95 px-12 md:px-48">
+        <div className="snap-start animation-timeline-view opacity-0 animate-easeIn">
+          <section>
+            <Title>What is LaughCraft</Title>
+            <Description />
+          </section>
+          <section>
+            <Title>Who is Nagisa</Title>
+            <About />
+          </section>
+          <section>
+            <Title>Value Proposition | LaughCraftが提供するもの</Title>
+            <Values />
+          </section>
+          <section>
+            <Title>Skills | できること</Title>
+            <Skills />
+          </section>
+          <section>
+            <Title>Works | 制作実績</Title>
+            <Projects />
+          </section>
+          <section>
+            <Title>Contact | お問い合わせ</Title>
+            <Contact />
+          </section>
+        </div>
+      </div>
+    </main>
   )
 }
--- components/title.tsx
+++ components/title.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 
 export const Title = ({ children }: { children: ReactNode }) => (
-  <h2 className="text-[#181411] text-[22px] font-bold leading-tight tracking-[-0.015em] py-4">
+  <h2 className="text-[#181411] text-[22px] font-bold leading-tight tracking-[-0.015em] py-4 text-center">
     {children}
   </h2>
 )
